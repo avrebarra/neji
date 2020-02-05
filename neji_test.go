a233b9e1f865bf76bbcca686d73c341a3ee9da64
Avreghly Barra <avreghly@gmail.com>
2020-02-05 19:23:03 +0700

    Add tests

---
+++ neji_test.go
@@ -0,0 +1,30 @@
+package neji_test
+
+import (
+	"github.com/shrotavre/neji"
+	"io/ioutil"
+	"testing"
+)
+
+func TestRecognize(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		// arrange
+		n, err := neji.NewRecognizer("./models")
+		imgData, err := ioutil.ReadFile("./fixtures/hinata.jpeg")
+		if err != nil {
+			t.Error("failed loading image test file")
+			return
+		}
+
+		// act
+		faces, err := n.RecognizeFaces(imgData, 10)
+
+		// assert
+		if err != nil {
+			t.Error("error recognizing faces")
+		}
+		if len(faces) != 1 {
+			t.Error("expected face count mismatch")
+		}
+	})
+}
